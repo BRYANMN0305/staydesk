899a5204a8f6f4db8bd1bc0ecdaef89ed8f42b76
Bryan <sabmunoz574@gmail.com>
2026-04-24 17:12:43 -0500

    cambio boton habitaciones

--- src/pages/Admin/Habitaciones.vue
+++ src/pages/Admin/Habitaciones.vue
@@ -27,7 +27,7 @@
             <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5">
               <path d="M12 5v14M5 12h14" />
             </svg>
-            Nueva habitación
+            Crear habitación
           </button>
         </div>
       </div>
